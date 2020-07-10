35b78731c904217df6b255ee79327bc1796abd8e
usmanarif24 <47153131+usmanarif24@users.noreply.github.com>
2020-07-10 16:30:39 +0400

    Connecting to Mongodb with Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+//connect database
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
+
+app.use("/api/users", require("./routes/api/users"));
 
 const PORT = process.env.PORT || 5000;
 
