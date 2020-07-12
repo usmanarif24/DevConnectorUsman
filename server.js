f5669ef105f6ee8d44c4093067eeb948d2d78154
usmanarif24 <47153131+usmanarif24@users.noreply.github.com>
2020-07-12 16:21:43 +0400

    Request % body Validation

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
 //connect database
 connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
 app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 
