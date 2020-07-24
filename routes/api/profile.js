8d979afc7cf18ea48065d95eba70493fb0312caf
usmanarif24 <47153131+usmanarif24@users.noreply.github.com>
2020-07-25 00:11:34 +0400

    Get profile by ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -117,4 +117,40 @@ router.post(
   }
 );
 
+//  @route    Get api/profile/
+//  @desc     Get all profiles
+//  @access   Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("User", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//  @route    Get api/profile/user/:user
+//  @desc     Get profile by user id
+//  @access   Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("User", ["name", "avatar"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
