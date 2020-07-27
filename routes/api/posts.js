0cdcfed5f90555da30d46963a5f6000578208977
usmanarif24 <47153131+usmanarif24@users.noreply.github.com>
2020-07-27 16:57:58 +0400

    Add Posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const Users = require("../../models/Users");
+const Post = require("../../models/Posts");
+const Profile = require("../../models/Profiles");
 
-//  @route    Get api/posts
-//  @desc     Test route
+//  @route    POST api/posts
+//  @desc     Create a post
 //  @access   Public
 
-router.get("/", (req, res) => res.send("posts route"));
+router.post(
+  "/",
+  [auth, [check("text", "text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    try {
+      const user = await Users.findById(req.user.id).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
