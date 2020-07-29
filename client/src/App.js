030f9877c6bf101b87145dfaa01a3e48b39baabd
usmanarif24 <47153131+usmanarif24@users.noreply.github.com>
2020-07-29 11:25:47 +0400

    React Setup and first video

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import "./App.css";
+import Navbar from "./Components/Layout/Navbar";
+import Landing from "./Components/Layout/Landing";
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Landing />
+  </Fragment>
+);
+
+export default App;
