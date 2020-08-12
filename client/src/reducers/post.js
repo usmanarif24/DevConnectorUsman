99ded2b71d7e78f655ff2ed5ba990fcccad41073
usmanarif24 <47153131+usmanarif24@users.noreply.github.com>
2020-08-12 23:36:25 +0400

    Add Comments

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,6 +2,8 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
   ADD_POST,
   DELETE_POST,
   GET_POST,
@@ -54,6 +56,23 @@ export default function (state = initialState, action) {
         posts: state.posts.map((post) =>
           post._id === payload.id ? { ...post, likes: payload.likes } : post
         ),
+        loading: false,
+      };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false,
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            (comment) => comment._id !== payload
+          ),
+        },
         loading: false,
       };
     default:
