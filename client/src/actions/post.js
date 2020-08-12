79215c0102e405abab75646ba44077cd67d12441
usmanarif24 <47153131+usmanarif24@users.noreply.github.com>
2020-08-12 22:54:47 +0400

    Single post display - Fixed

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -118,7 +118,7 @@ export const addPost = (FormData) => async (dispatch) => {
 //Get post
 export const getPost = (id) => async (dispatch) => {
   try {
-    const res = await axios.get(`api/posts/${id}`);
+    const res = await axios.get(`/api/posts/${id}`);
     dispatch({
       type: GET_POST,
       payload: res.data,
