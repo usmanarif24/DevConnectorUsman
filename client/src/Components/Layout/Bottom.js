16d64f4099e0b1c73f0925be3012bf1215f38181
usmanarif24 <47153131+usmanarif24@users.noreply.github.com>
2020-08-13 16:19:26 +0400

    Update - general fix

--- client/src/Components/Layout/Bottom.js
+++ client/src/Components/Layout/Bottom.js
@@ -1,13 +1,7 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const Bottom = (props) => {
-  return (
-    <div className='footer'>
-      Created by Syed Usman Arif after following the MERN course by Brad
-      Traversy. Made with MERN stack.
-    </div>
-  );
+const Bottom = () => {
+  return <div className='footer'>Created by Syed Usman Arif.</div>;
 };
 
 Bottom.propTypes = {};
